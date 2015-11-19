f1d5ccec9a7638b8d7c9c44c5445b4b875ab10fb
Axel Angel <axel+git@vneko.ch>
2015-11-19 22:18:12 +0100

    Clean, remove EOL spaces

--- Web/Authenticate/LDAP.hs
+++ Web/Authenticate/LDAP.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 -- | Module for using LDAP as an authentication service
--- 
--- This code was written for yesod-auth-ldap, but maybe it can be used in any 
+--
+-- This code was written for yesod-auth-ldap, but maybe it can be used in any
 -- given Haskell web framwork?
 -- For now, the only usage examples will be Yesod specific. So you can find
--- them in the yesod-auth-ldap repo 
+-- them in the yesod-auth-ldap repo
 
 module Web.Authenticate.LDAP
   ( loginLDAP
@@ -15,7 +15,7 @@ import Data.Text (Text,unpack)
 import LDAP
 import Control.Exception
 import Control.Monad.IO.Class
-  
+
 data LDAPAuthResult = Ok LDAPEntry
                     -- ^ Login successful
                     | NoSuchUser
@@ -31,7 +31,7 @@ instance Show LDAPAuthResult where
   show WrongPassword              = "Wrong password"
   show InitialBindFail            = "The initial bind attempt to the ldap" ++
                                     "server failed"
-   
+
 loginLDAP :: Text -> -- ^ query string (eg: uid=username or email=a@b.com)
              String -> -- ^ user's password
              String -> -- ^ LDAP URI
@@ -43,7 +43,7 @@ loginLDAP :: Text -> -- ^ query string (eg: uid=username or email=a@b.com)
 loginLDAP query pass ldapUri initDN initPassword searchDN ldapScope =
   do
    ldapOBJ <- ldapInitialize ldapUri
-   initBindResult <- try (ldapSimpleBind ldapOBJ initDN initPassword) 
+   initBindResult <- try (ldapSimpleBind ldapOBJ initDN initPassword)
                                                  :: IO (Either LDAPException ())
    case initBindResult of
      Right _ -> do -- Successful initial bind
@@ -53,7 +53,7 @@ loginLDAP query pass ldapUri initDN initPassword searchDN ldapScope =
                            (Just $ unpack query)
                            LDAPAllUserAttrs
                            False
--- FIXME y u no make new function for nested case statement?       
+-- FIXME y u no make new function for nested case statement?
        -- We try to bind with the dn of the returned entry
        case entries of
          [entry@(LDAPEntry dn _)] -> do
@@ -63,4 +63,4 @@ loginLDAP query pass ldapUri initDN initPassword searchDN ldapScope =
                                Right _ -> return $ Ok entry -- Successful user bind
                                Left _ -> return WrongPassword
          _               -> return NoSuchUser
-     Left _ -> return InitialBindFail    
+     Left _ -> return InitialBindFail
